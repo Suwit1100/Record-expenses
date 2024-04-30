eb99a888728eeef64719714f430d5d56b30d6395
Suwit1100 <suwitlichoeiwara@gmail.com>
2024-04-30 11:36:23 +0700

    import bootstrap กับ font

---
+++ record-expenses/src/components/Title.jsx
@@ -0,0 +1,15 @@
+function Title() {
+    return (
+        <>
+            <div className="row mt-5">
+                <div className="col-12 text-center">
+                    <h2>
+                        โปรแกรมบัญชีรายรับ-รายจ่าย
+                    </h2>
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default Title
