d19b1242224034f93de57317534011ed245d4ddc
Suwit1100 <suwitlichoeiwara@gmail.com>
2024-05-01 15:38:42 +0700

    Update Transaction.jsx

--- record-expenses/src/components/Transaction.jsx
+++ record-expenses/src/components/Transaction.jsx
@@ -3,21 +3,25 @@ import "./css/Transaction.css"
 
 function Transaction(props) {
     const { Items } = props;
+    const status = Items.amount > 0 ? "income" : "expense";
+    const symbo = Items.amount > 0 ? "+" : "-";
     return (
         <div className='container'>
             {Items.map((e) => {
+                const status = e.amount > 0 ? "income" : "expense";
+                const symbo = e.amount > 0 ? "+" : "-";
                 return (
-                    <div className="row d-flex align-items-center my-2" key={e.id}>
+                    <div className={`row d-flex align-items-center my-2 ${status}`} key={e.id}>
                         <div className="col-6">
                             {e.title}
                         </div>
                         <div className="col-6 text-end">
-                            {e.amount}
+                            {symbo}{e.amount}
                         </div>
                     </div>
                 );
             })};
-        </div>
+        </div >
     )
 }
 
