788d646f106df3e67c9d5e7663a8c8b0ad423aea
Suwit1100 <suwitlichoeiwara@gmail.com>
2024-05-01 14:04:43 +0700

    Update Forminsert.jsx

--- record-expenses/src/components/Forminsert.jsx
+++ record-expenses/src/components/Forminsert.jsx
@@ -1,11 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
+
 
 function Forminsert() {
+    const [title, SetTitle] = useState("")
+    const [amount, SetAmount] = useState(0)
+
+    const eventitle = (e) => {
+        console.log(e.target.value);
+        SetTitle(e.target.value)
+    }
+
+    const evenamount = (e) => {
+        console.log(e.target.value);
+        SetAmount(e.target.value)
+    }
+
+    const submit = (e) => {
+        e.preventDefault();
+        const Itemdata = {
+        }
+    }
+
+
+
     return (
         <>
-            <form>
-                <input type="text" placeholder='ชื่อรายการ' className='form-control my-2' />
-                <input type="text" placeholder='จำนวนเงิน' className='form-control my-2' />
+            <form onSubmit={submit}>
+                <input type="text" placeholder='ชื่อรายการ' className='form-control my-2' onChange={eventitle} />
+                <input type="text" placeholder='จำนวนเงิน' className='form-control my-2' onChange={evenamount} />
                 <input type="submit" className='form-control btn btn-success' value={"บันทึกรายการ"} />
             </form>
         </>
