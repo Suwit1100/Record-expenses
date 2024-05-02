f3fd73ac1b458a0fd29b2d3a7d7231b46577f606
Suwit1100 <suwitlichoeiwara@gmail.com>
2024-05-02 12:01:49 +0700

    ดัดการกรองค่าว่าง

--- record-expenses/src/components/Forminsert.jsx
+++ record-expenses/src/components/Forminsert.jsx
@@ -1,19 +1,20 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { v4 as uuidv4 } from 'uuid';
 
 
 function Forminsert(props) {
-    const [title, SetTitle] = useState("")
-    const [amount, SetAmount] = useState(0)
+    const [title, SetTitle] = useState("");
+    const [amount, SetAmount] = useState(0);
+    const [valid, SetValid] = useState(true);
 
     const eventitle = (e) => {
         console.log(e.target.value);
-        SetTitle(e.target.value)
+        SetTitle(e.target.value);
     }
 
     const evenamount = (e) => {
         console.log(e.target.value);
-        SetAmount(e.target.value)
+        SetAmount(e.target.value);
     }
 
     const submit = (e) => {
@@ -22,11 +23,20 @@ function Forminsert(props) {
             id: uuidv4(),
             title: title,
             amount: amount
-        }
+        };
         props.ItemSubmit(Itemdata);
         SetTitle("");
         SetAmount(0);
     }
+
+    useEffect(() => {
+        console.log("Call use Effect");
+        if (amount != 0 && title.trim().length > 0) {
+            SetValid(false);
+        } else {
+            SetValid(true);
+        }
+    }, [amount, title]);
 
 
 
@@ -35,7 +45,7 @@ function Forminsert(props) {
             <form onSubmit={submit}>
                 <input type="text" placeholder='ชื่อรายการ' className='form-control my-2' onChange={eventitle} value={title} />
                 <input type="text" placeholder='จำนวนเงิน' className='form-control my-2' onChange={evenamount} value={amount} />
-                <input type="submit" className='form-control btn btn-success' value={"บันทึกรายการ"} />
+                <input type="submit" className='form-control btn btn-success' value={"บันทึกรายการ"} disabled={valid} />
             </form>
         </>
     )
